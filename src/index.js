b396c139c25a7caa50e8aaf1860598e8e7bed3e9
Serjml <Serjml@list.ru>
2022-02-08 21:00:12 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, arr) {
+  let arr2 = arr.map(el => el.join(''))
+
+  for (let i = 0; i < arr.length; i){
+    if (str.includes(arr2[i])){
+      str = str.replace(arr2[i],'');
+      i = 0;
+    } else {
+      i++;
+    } 
+  }
+
+  if (str.length) {
+    return false
+  } else {
+    return true
+  }
 }
